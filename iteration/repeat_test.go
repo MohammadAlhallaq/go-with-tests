4362a4efa61f7ccbb255a86219e4fae5cf5391b1
MohammadAlhallaq <mohammad.alhallaq1@gmail.com>
2026-03-18 16:13:52 +0300

    wip

---
+++ iteration/repeat_test.go
@@ -0,0 +1,28 @@
+package iteration
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestRepeat(t *testing.T) {
+	repeated := Repeat("a", 10)
+	expected := "aaaaaaaaaa"
+
+	if repeated != expected {
+		t.Errorf("expected %q but got %q", expected, repeated)
+	}
+}
+
+func BenchmarkRepeat(b *testing.B) {
+	for b.Loop() {
+		Repeat("a", 10)
+	}
+}
+
+
+func ExampleRepeat() {
+	repeated := Repeat("b", 5)
+	fmt.Println(repeated)
+	// Output: bbbbb
+}
