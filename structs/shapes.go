98df2a8ecd8bed2cda42b3af0b386a14aafe3aae
MohammadAlhallaq <mohammad.alhallaq1@gmail.com>
2026-03-20 18:37:26 +0300

    wip

--- structs/shapes.go
+++ structs/shapes.go
@@ -7,16 +7,16 @@ type Shape interface {
 }
 
 type Rectangle struct {
-	Hight float64
-	Width float64
+	Height float64
+	Width  float64
 }
 
 func Perimeter(rectangle Rectangle) float64 {
-	return 2 * (rectangle.Width + rectangle.Hight)
+	return 2 * (rectangle.Width + rectangle.Height)
 }
 
 func (rectangle Rectangle) Area() float64 {
-	return rectangle.Hight * rectangle.Width
+	return rectangle.Height * rectangle.Width
 }
 
 type Circle struct {
@@ -25,4 +25,13 @@ type Circle struct {
 
 func (c Circle) Area() float64 {
 	return math.Pi * c.Radius * c.Radius
+}
+
+type Triangle struct {
+	Base   float64
+	Height float64
+}
+
+func (t Triangle) Area() float64 {
+	return (t.Base * t.Height) * 0.5
 }
