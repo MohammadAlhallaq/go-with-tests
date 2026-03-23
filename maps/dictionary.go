a8187c2ce441660567b3d3bec1597eca440c7caf
MohammadAlhallaq <mohammad.alhallaq1@gmail.com>
2026-03-23 21:52:39 +0300

    wip

--- maps/dictionary.go
+++ maps/dictionary.go
@@ -1,15 +1,18 @@
 package maps
 
-import (
-	"errors"
-)
-
 type Dictionary map[string]string
 
 var (
-	ErrNotFound   = errors.New("could not find the word you were looking for")
-	ErrWordExists = errors.New("cannot add word because it already exists")
+	ErrNotFound         = DictionaryErr("could not find the word you were looking for")
+	ErrWordExists       = DictionaryErr("cannot add word because it already exists")
+	ErrWordDoesNotExist = DictionaryErr("cannot perform operation on word because it does not exist")
 )
+
+type DictionaryErr string
+
+func (e DictionaryErr) Error() string {
+	return string(e)
+}
 
 func (d Dictionary) Search(word string) (string, error) {
 	definition, ok := d[word]
@@ -19,16 +22,46 @@ func (d Dictionary) Search(word string) (string, error) {
 	return definition, nil
 }
 
-func (d Dictionary) Add(key, value string) error {
-	_, err := d.Search(key)
+func (d Dictionary) Add(word, definition string) error {
+	_, err := d.Search(word)
 
 	switch err {
 	case ErrNotFound:
-		d[key] = value
+		d[word] = definition
 	case nil:
 		return ErrWordExists
 	default:
 		return err
 	}
+	return nil
+}
+
+func (d Dictionary) Update(word, definition string) error {
+
+	_, err := d.Search(word)
+
+	switch err {
+	case ErrNotFound:
+		return ErrWordDoesNotExist
+	case nil:
+		d[word] = definition
+	default:
+		return nil
+	}
+
+	return nil
+}
+
+func (d Dictionary) Delete(word string) error {
+	_, err := d.Search(word)
+	switch err {
+	case ErrNotFound:
+		return ErrWordDoesNotExist
+	case nil:
+		delete(d, word)
+	default:
+		return nil
+	}
+
 	return nil
 }
