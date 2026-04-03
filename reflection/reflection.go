9d04437924459c3b44e08d5f2e5520928576cc80
MohammadAlhallaq <mohammad.alhallaq1@gmail.com>
2026-04-03 16:45:22 +0300

    wip

---
+++ reflection/reflection.go
@@ -0,0 +1,30 @@
+package reflection
+
+import "reflect"
+
+func walk(x any, fn func(s string)) {
+	val := getValue(x)
+
+	switch val.Kind() {
+	case reflect.Struct:
+		for i := 0; i < val.NumField(); i++ {
+			walk(val.Field(i).Interface(), fn)
+		}
+	case reflect.Slice:
+		for i := 0; i < val.Len(); i++ {
+			walk(val.Index(i).Interface(), fn)
+		}
+	case reflect.String:
+		fn(val.String())
+	}
+}
+
+func getValue(x any) reflect.Value {
+	val := reflect.ValueOf(x)
+
+	if val.Kind() == reflect.Pointer {
+		return val.Elem()
+	}
+
+	return val
+}
---
+++ reflection/reflection_test.go
@@ -0,0 +1,74 @@
+package reflection
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestWalk(t *testing.T) {
+
+	type Profile struct {
+		Age  int
+		City string
+	}
+
+	type Person struct {
+		Name    string
+		Profile Profile
+	}
+
+	cases := []struct {
+		Name          string
+		Input         any
+		ExpectedCalls []string
+	}{
+		{
+			"struct with one string field",
+			struct {
+				Name string
+				Age  int
+			}{"Chris", 30},
+			[]string{"Chris"},
+		},
+
+		{
+			"nested fields",
+			Person{
+				"Chris",
+				Profile{33, "London"},
+			},
+			[]string{"Chris", "London"},
+		},
+
+		{
+			"pointers to things",
+			&Person{
+				"Chris",
+				Profile{33, "London"},
+			},
+			[]string{"Chris", "London"},
+		},
+
+		{
+			"slices",
+			[]Profile{
+				{33, "London"},
+				{34, "Reykjavík"},
+			},
+			[]string{"London", "Reykjavík"},
+		},
+	}
+
+	for _, test := range cases {
+		t.Run(test.Name, func(t *testing.T) {
+			var got []string
+			walk(test.Input, func(input string) {
+				got = append(got, input)
+			})
+
+			if !reflect.DeepEqual(got, test.ExpectedCalls) {
+				t.Errorf("got %v, want %v", got, test.ExpectedCalls)
+			}
+		})
+	}
+}
